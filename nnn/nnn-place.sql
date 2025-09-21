036890b5c084ee592de41aa6a5eed7c0e8683d4f
Derek Sivers <d@sive.rs>
2025-09-21 20:23:58 +1200

    wip

---
+++ nnn/nnn-place.sql
@@ -0,0 +1,130 @@
+-- input URI like "GB-ENG" or "SG"
+-- if it has a dash, that's COUNTRY-STATE
+create function nnn.place(_uri text, out body text) as $$
+declare
+	placename text;  --also pagetitle
+	pids integer[];  --people in this place
+	profiles1 jsonb; --public_id, name, title, pages:[{long, short}]
+	profiles2 jsonb; --public_id, name, pages:[{long, short}]
+	profiles3 jsonb; --name, title, pages:[{long, short}]
+	profiles4 jsonb; --name, pages:[{long, short}]
+begin
+	if substring($1, 3, 1) = '-' then
+		-- ids of people with now_pages in this country and state
+		select array(
+			select person_id
+			from now_pages
+			join people on now_pages.person_id = people.id
+			where people.country = split_part($1, '-', 1)
+			and people.state = split_part($1, '-', 2)
+		) into pids;
+		-- placename like Victoria, Australia
+		select concat(states.name, ', ', countries.name) into placename
+		from states
+		join countries on states.country = countries.code
+		where states.country = split_part($1, '-', 1)
+		and states.code = split_part($1, '-', 2);
+	else
+		-- ids of people with now_pages in this country
+		select array(
+			select person_id
+			from now_pages
+			join people on now_pages.person_id = people.id
+			where people.country = $1
+		) into pids;
+		-- placename like Singapore
+		select name into placename from countries where code = $1;
+	end if;
+	-- profiles that have a photo and all questions answered
+	profiles1 = coalesce((select jsonb_agg(r) from (
+		select people.name, now_profiles.public_id, now_profiles.title,
+		coalesce((select json_agg(r1) from (
+			select long, short
+			from now_pages
+			where now_pages.person_id = now_profiles.id
+			order by now_pages.id
+		) r1), '[]') as pages
+		from now_profiles
+		join people on now_profiles.id = people.id
+		where now_profiles.id = any(pids)
+		and photo is true
+		and length(title) > 0
+		and length(liner) > 0
+		and length(why) > 0
+		and length(thought) > 0
+		and length(red) > 0
+		order by people.id desc
+	) r), '[]');
+	-- profiles that have a photo but NOT all questions answered
+	profiles2 = coalesce((select jsonb_agg(r) from (
+		select people.name, now_profiles.public_id,
+		coalesce((select json_agg(r1) from (
+			select long, short
+			from now_pages
+			where now_pages.person_id = now_profiles.id
+			order by now_pages.id
+		) r1), '[]') as pages
+		from now_profiles
+		join people on now_profiles.id = people.id
+		where now_profiles.id = any(pids)
+		and photo is true and (
+			title is null or title = ''
+			or liner is null or liner = ''
+			or why is null or why = ''
+			or thought is null or thought = ''
+			or red is null or red = ''
+		)
+		order by people.id desc
+	) r), '[]');
+	-- profiles that LACK a photo but all questions answered
+	profiles3 = coalesce((select jsonb_agg(r) from (
+		select people.name, now_profiles.public_id, now_profiles.title,
+		coalesce((select json_agg(r1) from (
+			select long, short
+			from now_pages
+			where now_pages.person_id = now_profiles.id
+			order by now_pages.id
+		) r1), '[]') as pages
+		from now_profiles
+		join people on now_profiles.id = people.id
+		where now_profiles.id = any(pids)
+		and photo is false
+		and length(title) > 0
+		and length(liner) > 0
+		and length(why) > 0
+		and length(thought) > 0
+		and length(red) > 0
+		order by people.id desc
+	) r), '[]');
+	-- profiles that LACK a photo and NOT all questions answered
+	profiles4 = coalesce((select jsonb_agg(r) from (
+		select people.name, now_profiles.public_id,
+		coalesce((select json_agg(r1) from (
+			select long, short
+			from now_pages
+			where now_pages.person_id = now_profiles.id
+			order by now_pages.id
+		) r1), '[]') as pages
+		from now_profiles
+		join people on now_profiles.id = people.id
+		where now_profiles.id = any(pids)
+		and photo is false and (
+			title is null or title = ''
+			or liner is null or liner = ''
+			or why is null or why = ''
+			or thought is null or thought = ''
+			or red is null or red = ''
+		)
+		order by people.id desc
+	) r), '[]');
+	body = o.template('nnn-wrap', 'nnn-place', jsonb_build_object(
+		'placename', placename,
+		'pagetitle', '/now pages in ' || placename,
+		'date', current_date,
+		'profiles1', profiles1,
+		'profiles2', profiles2,
+		'profiles3', profiles3,
+		'profiles4', profiles4
+	));
+end;
+$$ language plpgsql;
