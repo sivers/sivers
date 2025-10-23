c2cda061a52f14049b843f550446f7335c44be53
Derek Sivers <derek@sivers.org>
2025-10-23 15:05:38 +1300

    feed XML

--- omni/feed.sql
+++ omni/feed.sql
@@ -1,4 +1,3 @@
--- linebreaks make it easier to test, but aren't necessary
 -- XML elements name lowercase unless quoted like "pubDate"
 create or replace function o.feed(_uri text, out xml text) as $$
 declare
@@ -9,21 +8,31 @@ begin
 	if f is null then
 		return;
 	end if;
-	xml = e'<?xml version="1.0"?>\n<rss version="2.0">\n<channel>\n';
-	xml = xml || xmlelement(name title, f.title) || e'\n';
-	xml = xml || xmlelement(name link, 'https://' || f.link) || e'\n';
-	xml = xml || xmlelement(name description, f.description) || e'\n';
+	xml = '<?xml version="1.0"?><rss version="2.0"><channel>';
+	xml = xml || xmlelement(name title, f.title);
+	xml = xml || xmlelement(name link, 'https://' || f.link);
+	xml = xml || xmlelement(name description, f.description);
+	xml = xml || xmlelement(name language, 'en-us');
+	xml = xml || xmlelement(name "lastBuildDate", o.rfc822(f.updated_at));
+	xml = xml || xmlelement(name "pubDate", o.rfc822(f.updated_at));
+	xml = xml || xmlelement(name ttl, '1440'); -- cache in minutes
 	for i in
 		select * from feeditems
 		where feed_uri = $1
 		order by pubdate desc, uri desc
 	loop
-		xml = xml || xmlelement(name item,
-			xmlelement(name title, i.title),
-			xmlelement(name "pubDate", to_char(i.pubdate at time zone 'UTC', 'Dy, DD Mon YYYY HH24:MI:SS "GMT"'))
-		) || e'\n';
+	xml = xml || xmlelement(name item,
+		xmlelement(name title, i.title),
+		xmlelement(name guid, xmlattributes('true' AS "isPermaLink"), 'https://' || i.uri),
+		xmlelement(name link, 'https://' || i.uri),
+		xmlelement(name author, 'Derek Sivers'),
+		xmlelement(name category, f.category), -- feeds.category
+		xmlelement(name "pubDate", o.rfc822(i.pubdate))
+	);
 	end loop;
-	xml = xml || e'</channel>\n</rss>';
+	xml = xml || '</channel></rss>';
+	-- linebreaks make it easier to test, but aren't necessary
+	xml = regexp_replace(xml, '><', e'>\n<', 'g');
 end;
 $$ language plpgsql;
 
--- omni/test/feed.sql
+++ omni/test/feed.sql
@@ -1,6 +1,7 @@
-insert into feeds (uri, link, title, description) values ('sive.rs/feed.rss', 'sive.rs/', 'a feed', 'a description');
-insert into feeditems (feed_uri, uri, title, pubdate) values ('sive.rs/feed.rss', 'sive.rs/one', 'one', '2025-10-22 12:34:56+00');
-insert into feeditems (feed_uri, uri, title, pubdate) values ('sive.rs/feed.rss', 'sive.rs/two', 'two', '2025-10-22 12:34:57+00');
+insert into feeds (uri, link, title, description, category, updated_at) values
+('sive.rs/feed.rss', 'sive.rs/', 'a feed', 'a description', 'a category', '2025-10-22 12:34:57+00');
+insert into feeditems (feed_uri, uri, title, content, pubdate) values ('sive.rs/feed.rss', 'sive.rs/one', 'one', 'One content, one!', '2025-10-22 12:34:56+00');
+insert into feeditems (feed_uri, uri, title, content, pubdate) values ('sive.rs/feed.rss', 'sive.rs/two', 'two', 'Two content, two!', '2025-10-22 12:34:57+00');
 
 select plan(1);
 
@@ -10,8 +11,26 @@ select is(xml, '<?xml version="1.0"?>
 <title>a feed</title>
 <link>https://sive.rs/</link>
 <description>a description</description>
-<item><title>two</title><pubDate>Wed, 22 Oct 2025 12:34:57 GMT</pubDate></item>
-<item><title>one</title><pubDate>Wed, 22 Oct 2025 12:34:56 GMT</pubDate></item>
+<language>en-us</language>
+<lastBuildDate>Wed, 22 Oct 2025 12:34:57 GMT</lastBuildDate>
+<pubDate>Wed, 22 Oct 2025 12:34:57 GMT</pubDate>
+<ttl>1440</ttl>
+<item>
+<title>two</title>
+<guid isPermaLink="true">https://sive.rs/two</guid>
+<link>https://sive.rs/two</link>
+<author>Derek Sivers</author>
+<category>a category</category>
+<pubDate>Wed, 22 Oct 2025 12:34:57 GMT</pubDate>
+</item>
+<item>
+<title>one</title>
+<guid isPermaLink="true">https://sive.rs/one</guid>
+<link>https://sive.rs/one</link>
+<author>Derek Sivers</author>
+<category>a category</category>
+<pubDate>Wed, 22 Oct 2025 12:34:56 GMT</pubDate>
+</item>
 </channel>
 </rss>')
 from o.feed('sive.rs/feed.rss');
