59757ad142cb2d6b6f5ec7f1e219509867855988
Derek Sivers <derek@sivers.org>
2025-11-06 21:24:40 +1300

    no need to shout

--- scripts/nownownow.rb
+++ scripts/nownownow.rb
@@ -1,32 +1,32 @@
 #!/usr/bin/env ruby
 # output nownownow.com static site
 require 'pg'
-DB = PG::Connection.new(dbname: 'sivers', user: 'sivers')
+db = PG::Connection.new(dbname: 'sivers', user: 'sivers')
 
-OUTDIR = '/var/www/html/nownownow.com/'
+outdir = '/var/www/html/nownownow.com/'
 
-r = DB.exec("select body from nnn.placespage()")[0]
-File.write(OUTDIR + 'index.html', r['body'])
+r = db.exec("select body from nnn.placespage()")[0]
+File.write(outdir + 'index.html', r['body'])
 
-DB.exec("select uri, body from nnn.placesout()").each do |r|
-  File.write(OUTDIR + r['uri'], r['body'])
+db.exec("select uri, body from nnn.placesout()").each do |r|
+  File.write(outdir + r['uri'], r['body'])
 end
 
-r = DB.exec("select body from nnn.random()")[0]
-File.write(OUTDIR + 'random', r['body'])
+r = db.exec("select body from nnn.random()")[0]
+File.write(outdir + 'random', r['body'])
 
-r = DB.exec("select body from nnn.now()")[0]
-File.write(OUTDIR + 'now', r['body'])
+r = db.exec("select body from nnn.now()")[0]
+File.write(outdir + 'now', r['body'])
 
-r = DB.exec("select body from nnn.about()")[0]
-File.write(OUTDIR + 'about', r['body'])
+r = db.exec("select body from nnn.about()")[0]
+File.write(outdir + 'about', r['body'])
 
-r = DB.exec("select body from nnn.text()")[0]
-File.write(OUTDIR + 'nownownow.txt', r['body'])
+r = db.exec("select body from nnn.text()")[0]
+File.write(outdir + 'nownownow.txt', r['body'])
 
-%x(mkdir -p #{OUTDIR}/p)
-%x(rm -f #{OUTDIR}/p/????)
-DB.exec("select uri, body from nnn.profiles()").each do |r|
-  File.write(OUTDIR + 'p/' + r['uri'], r['body'])
+%x(mkdir -p #{outdir}/p)
+%x(rm -f #{outdir}/p/????)
+db.exec("select uri, body from nnn.profiles()").each do |r|
+  File.write(outdir + 'p/' + r['uri'], r['body'])
 end
 
