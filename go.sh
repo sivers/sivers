aa5c8b138c51c35edf2b17d78f770157f199ca6a
Derek Sivers <derek@sivers.org>
2026-03-01 18:50:23 +1300

    blast init

--- go.sh
+++ go.sh
@@ -1,25 +1,16 @@
 #!/bin/ksh
 # ./go.sh on live server to compile binaries, move into place, and start
 
-# usually the directory name is same as the Go filename
-# but when different, use dirname/base-filename
-set -A list fed mynow nnn scripts/listener
+set -A list blast fed mynow nnn
 
 for item in ${list[@]}; do
-	if [[ $item == */* ]]; then
-		d=${item%/*}
-		f=${item#*/}
-	else
-		d=$item
-		f=$item
-	fi
-	cd $d
-	go build $f.go
-	doas mv -f $f /usr/local/bin/
+	cd $item
+	go build
+	doas mv -f $item /usr/local/bin/
 	cd ..
-	doas cp rc.d/$f /etc/rc.d/
-	doas chmod 0555 /etc/rc.d/$f
-	doas rcctl enable $f
-	doas rcctl restart $f
+	doas cp rc.d/$item /etc/rc.d/
+	doas chmod 0555 /etc/rc.d/$item
+	doas rcctl enable $item
+	doas rcctl restart $item
 done
 
