2ade8eece9bc787f87355c48cdb974f0524911d9
Derek Sivers <derek@sivers.org>
2026-03-09 07:14:38 +1300

    separate

--- ding/ding.go
+++ ding/ding.go
@@ -2,91 +2,11 @@ package main
 
 import (
 	"fmt"
-	"github.com/lib/pq"
 	"log"
 	"net/http"
 	"os"
-	"os/signal"
 	"sive.rs/sivers/internal/xx"
-	"strconv"
-	"syscall"
-	"time"
 )
-
-func Router() *http.ServeMux {
-	mux := http.NewServeMux()
-
-	// activitypub.go
-	mux.HandleFunc("GET /d", apProfileOrWeb)
-	mux.HandleFunc("GET /d/", apDeSlash)
-	mux.HandleFunc("GET /d/outbox", apOutbox)
-	mux.HandleFunc("GET /d/followers", apFollowers)
-	mux.HandleFunc("GET /d/posts/{id}", apPost1)
-	mux.HandleFunc("POST /d/inbox", apInbox)
-
-	return mux
-}
-
-// PostgreSQL LISTEN for NOTIFY channels that need to be named in 3 places, below:
-func Listener() {
-	lq := pq.NewListener(xx.DSN,
-		10*time.Second,
-		time.Minute,
-		func(ev pq.ListenerEventType, err error) {
-			if err != nil {
-				log.Printf("NewListener error: %v (err=%v)\n", ev, err)
-			}
-		},
-	)
-
-	// NOTIFY NAMES HERE
-	if err := lq.Listen("email2send"); err != nil {
-		log.Fatalf("Listener failed: %v", err)
-	}
-	if err := lq.Listen("newtweet"); err != nil {
-		log.Fatalf("Listener failed: %v", err)
-	}
-
-	done := make(chan os.Signal, 1)
-	signal.Notify(done, os.Interrupt, syscall.SIGTERM)
-
-	for {
-		select {
-		case n := <-lq.Notify:
-			if n == nil {
-				continue
-			}
-			log.Printf("Listener heard channel=%s pid=%d payload=%s\n", n.Channel, n.BePid, n.Extra)
-			switch n.Channel {
-			// NOTIFY NAMES HERE
-			case "email2send":
-				id, _ := strconv.Atoi(n.Extra)
-				log.Printf("SENDING EMAIL: %d", id)
-				go DBMail(id)
-			case "newtweet":
-				id, _ := strconv.Atoi(n.Extra)
-				var tw Tweet
-				err := xx.DB.QueryRow("select id, time, message from tweets where id = $1", id).Scan(&tw.ID, &tw.Time, &tw.Message)
-				if err == nil {
-					log.Printf("SENDING TWEET: %s", tw.Message)
-					//go Toot(tw)
-					//go Bloop(tw)
-					//go Xeet(tw)
-				}
-			}
-
-		case <-time.After(90 * time.Second):
-			go func() { _ = lq.Ping() }()
-
-		case <-done:
-			// NOTIFY NAMES HERE
-			_ = lq.Unlisten("email2send")
-			_ = lq.Unlisten("newtweet")
-			_ = lq.Close()
-			return
-		}
-	}
-}
 
 func main() {
 	f, err := os.OpenFile("/tmp/ding.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
@@ -108,10 +28,11 @@ func main() {
 	}
 
 	// PostgreSQL
-	go Listener()
+	go listener()
 
 	// HTTP
-	mux := Router()
-	fmt.Println("ding server starting on :8080")
-	http.ListenAndServe(":8080", mux)
+	mux := router()
+	fmt.Println("ding server starting on :2407")
+	http.ListenAndServe(":2407", mux)
 }
+
---
+++ ding/listener.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"github.com/lib/pq"
+	"log"
+	"os"
+	"os/signal"
+	"sive.rs/sivers/internal/xx"
+	"strconv"
+	"syscall"
+	"time"
+)
+
+// PostgreSQL LISTEN for NOTIFY channels that need to be named in 3 places, below:
+func listener() {
+	lq := pq.NewListener(xx.DSN,
+		10*time.Second,
+		time.Minute,
+		func(ev pq.ListenerEventType, err error) {
+			if err != nil {
+				log.Printf("NewListener error: %v (err=%v)\n", ev, err)
+			}
+		},
+	)
+
+	// NOTIFY NAMES HERE
+	if err := lq.Listen("email2send"); err != nil {
+		log.Fatalf("Listener failed: %v", err)
+	}
+	if err := lq.Listen("newtweet"); err != nil {
+		log.Fatalf("Listener failed: %v", err)
+	}
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGTERM)
+
+	for {
+		select {
+		case n := <-lq.Notify:
+			if n == nil {
+				continue
+			}
+			log.Printf("Listener heard channel=%s pid=%d payload=%s\n", n.Channel, n.BePid, n.Extra)
+			switch n.Channel {
+			// NOTIFY NAMES HERE
+			case "email2send":
+				id, _ := strconv.Atoi(n.Extra)
+				log.Printf("SENDING EMAIL: %d", id)
+				go DBMail(id)
+			case "newtweet":
+				id, _ := strconv.Atoi(n.Extra)
+				var tw Tweet
+				err := xx.DB.QueryRow("select id, time, message from tweets where id = $1", id).Scan(&tw.ID, &tw.Time, &tw.Message)
+				if err == nil {
+					log.Printf("SENDING TWEET: %s", tw.Message)
+					//go Toot(tw)
+					//go Bloop(tw)
+					//go Xeet(tw)
+				}
+			}
+
+		case <-time.After(90 * time.Second):
+			go func() { _ = lq.Ping() }()
+
+		case <-done:
+			// NOTIFY NAMES HERE
+			_ = lq.Unlisten("email2send")
+			_ = lq.Unlisten("newtweet")
+			_ = lq.Close()
+			return
+		}
+	}
+}
