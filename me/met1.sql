a8f063c6a5b9a79f5cf9bfab75b1bc493b201af7
Derek Sivers <derek@sivers.org>
2026-03-23 20:53:40 +1300

    met1

---
+++ me/met1.sql
@@ -0,0 +1,35 @@
+-- sive.rs/met/:id
+
+create function me.met1(_id integer, out body text) as $$
+declare
+	p record;
+	met1urls jsonb;
+begin
+	select people.name,
+	meetwheres.location, meetwheres.display,
+	meetings.whatime, meetings.notes
+	from meetings into p
+	join people on meetings.person_id = people.id
+	join meetwheres on meetings.where_id = meetwheres.id
+	where meetings.id = $1;
+
+	select jsonb_agg(r) into met1urls from (
+		select urls.url
+		from meetings
+		join urls on meetings.person_id = urls.person_id
+		where meetings.id = $1
+		order by urls.main desc nulls last, urls.id
+	) r;
+
+	body = o.template('me-wrap', 'me-met1', jsonb_build_object(
+		'pagetitle', p.name || ' met with Derek Sivers',
+		'name', p.name,
+		'location', p.location,
+		'display', p.display,
+		'whatime', p.whatime,
+		'notes', p.notes,
+		'urls', met1urls
+	));
+end;
+$$ language plpgsql;
+
---
+++ me/test/met1.sql
@@ -0,0 +1,31 @@
+insert into templates (code, template) values ('me-wrap', '<title>{{pagetitle}}</title><body>{{{core}}}</body>');
+insert into templates (code, template) values ('me-met1', '<h1>{{name}}</h1>
+location:{{location}}
+display:{{display}}
+whatime:{{whatime}}
+notes:{{notes}}
+{{#urls}}
+url:{{url}}
+{{/urls}}
+');
+insert into meetwheres (id, location, display) values (1, 'One Hotel', '2025-01 - One Place');
+
+insert into people (id, name) values (1, 'Mrs. One');
+
+insert into urls (person_id, url, main) values (1, 'https://last.url', null);
+insert into urls (person_id, url, main) values (1, 'https://first.url', true);
+insert into urls (person_id, url, main) values (1, 'https://middle.url', false);
+
+insert into meetings (id, where_id, person_id, whatime, topics, notes) values (1, 1, 1, '2025-01-15 12:00:00+00', 'topics one', 'notes one');
+
+select plan(1);
+select is(body, '<title>Mrs. One met with Derek Sivers</title><body><h1>Mrs. One</h1>
+location:One Hotel
+display:2025-01 - One Place
+whatime:2025-01-15T12:00:00+00:00
+notes:notes one
+url:https://first.url
+url:https://middle.url
+url:https://last.url
+</body>')
+from me.met1(1);
