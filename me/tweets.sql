0c39d4a7290fb4e033de99fe1808e9e97899d75b
Derek Sivers <derek@sivers.org>
2026-04-11 18:37:28 +1200

    amp

--- me/tweets.sql
+++ me/tweets.sql
@@ -3,7 +3,7 @@ begin
 	body = o.template('me-wrap', 'me-tweets', jsonb_build_object(
 		'pagetitle', 'Derek Sivers tweets, microposts',
 		'tweets', (select jsonb_agg(r) from (
-			select time::date as ymd, o.hyperlink(message) as tweet
+			select time::date as ymd, o.hyperlink(replace(message, '&', '&amp;')) as tweet
 			from tweets
 			where article_id is null and time <= now()
 			order by time desc nulls last
