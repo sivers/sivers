1748fa13c67b2225e50efbbbf2ce687aefba3716
Derek Sivers <derek@sivers.org>
2026-04-23 07:49:16 +1200

    tweets and topics in sitemap

--- me/sitemap.sql
+++ me/sitemap.sql
@@ -1,8 +1,4 @@
--- I was hoping to use the *_uris() functions here
--- but since they only have the URIs and I also need the lastmod dates
--- I instead just duplicated their 'where' conditions. Maybe unwise.
---
--- NOTE: 2026-04-12 is the day I relaunched the whole site
+-- 2026-04-22 is the day I relaunched the whole site
 create function me.sitemap(out body text) as $$
 declare
 	r record;
@@ -11,13 +7,22 @@ begin
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 <url><loc>https://sive.rs/</loc><lastmod>' || current_date || '</lastmod></url>
 ';
+	body = body || (select '<url><loc>https://sive.rs/d</loc><lastmod>' || max(time::date) || e'</lastmod></url>\n' from tweets);
 	for r in select uri
 		from me.pages()
+		where uri not in (select uri from metabooks) -- metabooks are in topic, next
 		order by uri
 		loop
-		body = body || '<url><loc>https://sive.rs/' || r.uri || e'</loc><lastmod>2026-04-12</lastmod></url>\n';
+		body = body || '<url><loc>https://sive.rs/' || r.uri || e'</loc><lastmod>2026-04-22</lastmod></url>\n';
 	end loop;
-	for r in select uri, greatest(posted, '2026-04-12'::date) as posted
+	for r in select topic, greatest(max(posted), '2026-04-22'::date) as posted
+		from articles
+		group by topic
+		order by topic
+		loop
+		body = body || '<url><loc>https://sive.rs/' || r.topic || '</loc><lastmod>' || r.posted || e'</lastmod></url>\n';
+	end loop;
+	for r in select uri, greatest(posted, '2026-04-22'::date) as posted
 		from articles
 		where posted is not null
 		and posted <= now()
@@ -25,7 +30,7 @@ begin
 		loop
 		body = body || '<url><loc>https://sive.rs/' || r.uri || '</loc><lastmod>' || r.posted || e'</lastmod></url>\n';
 	end loop;
-	for r in select code, greatest(read, '2026-04-12'::date) as read
+	for r in select code, greatest(read, '2026-04-22'::date) as read
 		from ebooks
 		where read is not null
 		and rating is not null
@@ -34,7 +39,7 @@ begin
 		loop
 		body = body || '<url><loc>https://sive.rs/book/' || r.code || '</loc><lastmod>' || r.read || e'</lastmod></url>\n';
 	end loop;
-	for r in select uri, greatest(ymdhm::date, '2026-04-12'::date) as ymd
+	for r in select uri, greatest(ymdhm::date, '2026-04-22'::date) as ymd
 		from interviews
 		where uri is not null
 		and summary is not null
@@ -42,7 +47,7 @@ begin
 		loop
 		body = body || '<url><loc>https://sive.rs/' || r.uri || '</loc><lastmod>' || r.ymd || e'</lastmod></url>\n';
 	end loop;
-	for r in select where_id, greatest(max(whatime)::date, '2026-04-12'::date) as lastmod
+	for r in select where_id, greatest(max(whatime)::date, '2026-04-22'::date) as lastmod
 		from meetings
 		where whatime < now()
 		and topics is not null
@@ -51,7 +56,7 @@ begin
 		loop
 		body = body || '<url><loc>https://sive.rs/met/at-' || r.where_id || '</loc><lastmod>' || r.lastmod || e'</lastmod></url>\n';
 	end loop;
-	for r in select id, greatest(whatime::date, '2026-04-12'::date) as lastmod
+	for r in select id, greatest(whatime::date, '2026-04-22'::date) as lastmod
 		from meetings
 		where whatime < now()
 		and topics is not null
@@ -63,7 +68,7 @@ begin
 		from presentations
 		order by month desc
 		loop
-		body = body || '<url><loc>https://sive.rs/' || r.uri || e'</loc><lastmod>2026-04-12</lastmod></url>\n';
+		body = body || '<url><loc>https://sive.rs/' || r.uri || e'</loc><lastmod>2026-04-22</lastmod></url>\n';
 	end loop;
 	body = body || '</urlset>';
 end;
