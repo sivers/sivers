09d840d7ba1e78c7a5ff669606095c7013c5067e
Derek Sivers <derek@sivers.org>
2026-05-08 13:27:13 +1200

    all

--- me/tweets.sql
+++ me/tweets.sql
@@ -4,8 +4,7 @@ create function me.tweets(out body text) as $$
 		'tweets', (select jsonb_agg(r) from (
 			select time::date as ymd, o.hyperlink(replace(message, '&', '&amp;')) as tweet
 			from tweets
-			where article_id is null and time <= now()
-			order by time desc nulls last
+			order by time desc
 		) r)
 	));
 $$ language sql;
