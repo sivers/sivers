859f435249cd30ca96808f410b7b3cbd859a3aec
Derek Sivers <derek@sivers.org>
2026-05-13 06:43:57 +1200

    css in header

--- me/sitemap.sql
+++ me/sitemap.sql
@@ -1,4 +1,4 @@
--- 2026-04-22 is the day I relaunched the whole site
+-- 2026-05-13 I changed headers so updated date
 create function me.sitemap(out body text) as $$
 declare
 	r record;
@@ -14,16 +14,16 @@ begin
 		where uri not in (select uri from metabooks) -- metabooks are in topic, next
 		order by uri
 		loop
-		body = body || '<url><loc>https://sive.rs/' || r.uri || e'</loc><lastmod>2026-04-22</lastmod></url>\n';
+		body = body || '<url><loc>https://sive.rs/' || r.uri || e'</loc><lastmod>2026-05-13</lastmod></url>\n';
 	end loop;
-	for r in select topic, greatest(max(posted), '2026-04-22'::date) as posted
+	for r in select topic, greatest(max(posted), '2026-05-13'::date) as posted
 		from articles
 		group by topic
 		order by topic
 		loop
 		body = body || '<url><loc>https://sive.rs/' || r.topic || '</loc><lastmod>' || r.posted || e'</lastmod></url>\n';
 	end loop;
-	for r in select uri, greatest(posted, '2026-04-22'::date) as posted
+	for r in select uri, greatest(posted, '2026-05-13'::date) as posted
 		from articles
 		where posted is not null
 		and posted <= now()
@@ -31,7 +31,7 @@ begin
 		loop
 		body = body || '<url><loc>https://sive.rs/' || r.uri || '</loc><lastmod>' || r.posted || e'</lastmod></url>\n';
 	end loop;
-	for r in select code, greatest(read, '2026-04-22'::date) as read
+	for r in select code, greatest(read, '2026-05-13'::date) as read
 		from ebooks
 		where read is not null
 		and rating is not null
@@ -40,7 +40,7 @@ begin
 		loop
 		body = body || '<url><loc>https://sive.rs/book/' || r.code || '</loc><lastmod>' || r.read || e'</lastmod></url>\n';
 	end loop;
-	for r in select uri, greatest(ymdhm::date, '2026-04-22'::date) as ymd
+	for r in select uri, greatest(ymdhm::date, '2026-05-13'::date) as ymd
 		from interviews
 		where uri is not null
 		and summary is not null
@@ -48,7 +48,7 @@ begin
 		loop
 		body = body || '<url><loc>https://sive.rs/' || r.uri || '</loc><lastmod>' || r.ymd || e'</lastmod></url>\n';
 	end loop;
-	for r in select where_id, greatest(max(whatime)::date, '2026-04-22'::date) as lastmod
+	for r in select where_id, greatest(max(whatime)::date, '2026-05-13'::date) as lastmod
 		from meetings
 		where whatime < now()
 		and topics is not null
@@ -57,7 +57,7 @@ begin
 		loop
 		body = body || '<url><loc>https://sive.rs/met/at-' || r.where_id || '</loc><lastmod>' || r.lastmod || e'</lastmod></url>\n';
 	end loop;
-	for r in select id, greatest(whatime::date, '2026-04-22'::date) as lastmod
+	for r in select id, greatest(whatime::date, '2026-05-13'::date) as lastmod
 		from meetings
 		where whatime < now()
 		and topics is not null
@@ -69,7 +69,7 @@ begin
 		from presentations
 		order by month desc
 		loop
-		body = body || '<url><loc>https://sive.rs/' || r.uri || e'</loc><lastmod>2026-04-22</lastmod></url>\n';
+		body = body || '<url><loc>https://sive.rs/' || r.uri || e'</loc><lastmod>2026-05-13</lastmod></url>\n';
 	end loop;
 	body = body || '</urlset>';
 end;
