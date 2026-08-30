4299261ea818fa8a610ec64e966a26b3919fae20
Derek Sivers <derek@sivers.org>
2026-08-31 08:32:21 +1200

    blog comments whitelist only known email address

--- me/comment_post.sql
+++ me/comment_post.sql
@@ -1,8 +1,16 @@
 create function me.comment_post(_formdata jsonb,
 	out head text, out body text) as $$
 declare
+	_email text;
+	_comment text;
 	pid integer;
+	pname text;
 begin
+	-- clean email: lowercase and no whitespace
+	_email = o.clean_email($1->>'email');
+	-- clean comment: remove \r and tags
+	_comment = regexp_replace(replace($1->>'comment', e'\r', ''), '</?[^>]+?>', '', 'g');
+
 	-- Form submitted URI not in my URIs? Stop now
 	perform 1 from articles where uri = $1->>'uri';
 	if not found then
@@ -10,42 +18,34 @@ begin
 		return;
 	end if;
 
-	-- Location: /thanks as a hard fail clear intention to spam
-	if lower(regexp_replace($1->>'name', '\s+', '', 'g')) ~ '^test$|crypto' or
-	lower($1->>'email') ~ '^test@|@example' then
-		head = e'303\r\nLocation: /thanks';
-		return;
-	end if;
-
-	-- comment looks like junk (has a-z but lacks spaces)
-	if ($1->>'comment' ~ '[a-zA-Z]{3}') is true and ($1->>'comment' ~ '(.* ){2}') is false then
-		head = e'303\r\nLocation: /thanks';
-		return;
-	end if;
-
-	-- this function only fails (returning null) if name or email is malformed
-	select x.pid into pid
-	from o.person_create($1->>'name', $1->>'email') x;
+	-- Email address known?
+	select person_id into pid from ats where email = _email;
 	if pid is null then
-		head = e'303\r\nLocation: /' || $1->>'uri';
-		return;
+		-- NO! UNKNOWN. Show email and comment, telling to contact me
+		body = o.template('me-wrap', 'me-commentno', jsonb_build_object(
+			'uri', $1->>'uri',
+			'email', _email,
+			'comment', _comment
+		));
+	else
+		-- YES! KNOWN 
+		-- log
+		update ats set used = now() where email = email;
+		perform o.iplog(pid, ($1->>'ip')::inet);
+
+		-- "greeting" = usually their first name
+		select greeting into pname from people where id = pid;
+
+		-- new comment sends notify, so ding rewrites the article page
+		insert into comments (person_id, uri, name, email, comment)
+		values (pid, $1->>'uri', pname, _email, _comment);
+
+		-- say thanks. link to article, articles, contact, home.
+		body = o.template('me-wrap', 'me-commentpost', jsonb_build_object(
+			'uri', $1->>'uri',
+			'name', pname
+		));
 	end if;
-
-	insert into comments (person_id, uri, name, email, comment)
-	values (pid,
-		$1->>'uri',
-		o.clean_name($1->>'name'),
-		o.clean_email($1->>'email'),
-		regexp_replace($1->>'comment', '</?[^>]+?>', '', 'g'));
-
-	update ats set used = now() where email = o.clean_email($1->>'email');
-
-	perform o.iplog(pid, ($1->>'ip')::inet);
-
-	body = o.template('me-wrap', 'me-commentpost', jsonb_build_object(
-		'uri', $1->>'uri',
-		'name', o.clean_name($1->>'name')
-	));
 end;
 $$ language plpgsql;
 
--- me/test/comment_post.sql
+++ me/test/comment_post.sql
@@ -1,12 +1,13 @@
 insert into countries (code, name) values ('GB', 'Great Britain');
 insert into templates (code, template) values ('me-wrap', '<html>{{{core}}}</html>');
-insert into templates (code, template) values ('me-commentpost', 'uri={{uri}},name={{name}}');
+insert into templates (code, template) values ('me-commentpost', 'OK uri={{uri}},name={{name}}');
+insert into templates (code, template) values ('me-commentno', 'NO uri={{uri}},email={{email}},comment={{{comment}}}');
 
 insert into topics (uri) values ('test');
 insert into articles (topic, uri, posted, title, original) values ('test', 'apost', '2026-04-01', 'A Post', '<p>This is a post.</p>');
 
-insert into people (id, name) values (1, 'Past Poster');
-insert into ats (person_id, email) values (1, 'past@poster.com');
+insert into people (id, name, greeting) values (1, 'Past Poster', 'Posterman');
+insert into ats (person_id, email, used) values (1, 'past@poster.com', '2026-04-01');
 insert into comments (id, person_id, uri, name, email, comment) values (1, 1, 'apost', 'Past Poster', 'past@poster.com', 'Past comment.');
 
 insert into ips (range, country, state, city) values ('[16843008,16843264)', 'GB', 'ENG', 'Oxford'); -- 1.1.1.1
@@ -14,81 +15,44 @@ insert into ips (range, country, state, city) values ('[16843008,16843264)', 'GB
 select setval('people_id_seq', 1);
 select setval('comments_id_seq', 1);
 
-select plan(19);
+select plan(12);
 select is(count(*)::integer, 1, 'one comment') from comments;
 
+---- A GOOD POST:
+
 select is(head, null, 'empty head'),
-	is(body, '<html>uri=apost,name=Willy Wonka</html>', 'basic body')
+	is(body, '<html>OK uri=apost,name=Posterman</html>', 'posted ok')
 from me.comment_post(jsonb_build_object(
 	'uri', 'apost',
-	'name', '  Willy Wonka',
-	'email', 'WILLY@WONKA.COM',
-	'comment', 'Chocolate is good.',
+	'email', ' PAST @ POSTER . COM ',
+	'comment', 'A <strong>new</strong> comment.',
 	'ip', '1.1.1.1'));
 
 select is(count(*)::integer, 2, 'new comment') from comments;
-
-select is(name, 'Willy Wonka', 'name'),
-	is(ats.email, 'willy@wonka.com', 'ats.email')
-from people join ats on people.id = ats.person_id where people.id = 2;
+select is(uri, 'apost', 'uri match'),
+	is(person_id, 1, 'person_id match'),
+	is(name, 'Posterman', 'got name from db'),
+	is(comment, 'A new comment.', 'cleaned comment')
+from comments order by id desc limit 1;
 
 select is(statvalue, 'Oxford', 'ip saved location')
 from stats where person_id = 2 and statkey = 'city';
 
-select is(uri, 'apost'),
-	is(name, 'Willy Wonka'),
-	is(email, 'willy@wonka.com'),
-	is(comment, 'Chocolate is good.')
-from comments where id = 2;
+select isnt(used, '2026-04-01', 'ats.used updated')
+from ats where email = 'past@poster.com';
 
--- Now let's do some bad ones:
+-- BAD POSTS:
+
 select is(head, e'303\r\nLocation: /thanks', 'wrong uri')
 from me.comment_post(jsonb_build_object(
 	'uri', 'xx',
-	'name', 'Willy Wonka',
 	'email', 'willy@wonka.com',
 	'comment', 'Chocolate is good.'));
 
-select is(head, e'303\r\nLocation: /thanks', 'crypto name')
+select is(head, null, 'empty head'),
+	is(body, '<html>NO uri=apost,email=willy@wonka.com,comment=A new comment.</html>', 'cleaned,refused')
 from me.comment_post(jsonb_build_object(
 	'uri', 'apost',
-	'name', 'Hot Crypto Now',
-	'email', 'willy@wonka.com',
-	'comment', 'Chocolate is good.'));
-
-select is(head, e'303\r\nLocation: /thanks', 'test name')
-from me.comment_post(jsonb_build_object(
-	'uri', 'apost',
-	'name', 'test',
-	'email', 'willy@wonka.com',
-	'comment', 'Chocolate is good.'));
-
-select is(head, e'303\r\nLocation: /thanks', 'test@ email')
-from me.comment_post(jsonb_build_object(
-	'uri', 'apost',
-	'name', 'Willy Wonka',
-	'email', 'test@wonka.com',
-	'comment', 'Chocolate is good.'));
-
-select is(head, e'303\r\nLocation: /thanks', '@example email')
-from me.comment_post(jsonb_build_object(
-	'uri', 'apost',
-	'name', 'Willy Wonka',
-	'email', 'willy@example.com',
-	'comment', 'Chocolate is good.'));
-
-select is(head, e'303\r\nLocation: /thanks', 'junk comment')
-from me.comment_post(jsonb_build_object(
-	'uri', 'apost',
-	'name', 'Willy Wonka',
-	'email', 'willy@wonka.com',
-	'comment', 'Chocolateisgood.'));
-
-select is(head, null, 'Chinese comment OK'),
-	is(body, '<html>uri=apost,name=Willy Wonka</html>')
-from me.comment_post(jsonb_build_object(
-	'uri', 'apost',
-	'name', 'Willy Wonka',
-	'email', 'willy@wonka.com',
-	'comment', '巧克力真好吃。OK!'));
+	'email', ' willy@WONKA.com ',
+	'comment', 'A <strong>new</strong> comment.'));
 
