517261f33b2d27e58110a67cf077f1a8ae79a55d
Derek Sivers <derek@sivers.org>
2026-09-21 13:57:51 +1200

    hyperlink links URLs without http prefix

--- o/hyperlink.sql
+++ o/hyperlink.sql
@@ -1,8 +1,22 @@
 -- * input: 'Look: https://example.com/ I like it!'
 -- * output: 'Look: <a href="https://example.com/">example.com/</a> I like it!'
-create function o.hyperlink(text) returns text as $$
-	select regexp_replace($1,
-        '(https://([^\s]+))',
-        '<a href="\1">\2</a>',
-        'g');
+create or replace function o.hyperlink(text) returns text as $$
+	select regexp_replace(o.escape_html($1),
+        $regex$
+            (?<![a-z0-9_@./-])      # Do not start inside a domain or email
+            (                       # Group 1: original visible URL
+                (?:https?://)?      # Optional existing scheme
+                (                   # Group 2: domain and path, without scheme
+                    (?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+
+                    [a-z]{2,63}     # Alphabetic top-level domain
+                    (?:/
+                        (?:[a-z0-9._~%/-]*[a-z0-9_~%/-])?
+                    )?              # Optional path, not ending in a period
+                )
+            )
+            (?![a-z0-9_@-]|\.[a-z0-9_-])
+        $regex$,
+        $html$<a href="https://\2">\2</a>$html$,
+	'gix');
 $$ language sql immutable strict parallel safe;
+
